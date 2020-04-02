f4bb306fda71efbe8b161878964278e008125dcf
Dante L'Erario <dantelerario@gmail.com>
2020-04-02 18:48:08 +0200

    save, script 1-5 doesnt work

--- js/main.js
+++ js/main.js
@@ -2,39 +2,35 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-var parola = prompt('Inserire parola');
+// var parola = prompt('Inserire parola');
+//
+// var result = palindromo(parola);
+//
+// console.log(result);
 
-var result = palindromo(parola);
-
-console.log(result);
-
-function palindromo(pal) {
-
-  // var reverse = pal.split('').reverse().join('');
-  //
-  // if (reverse === pal) {
-  //
-  //   return ('Palindromo = ') + reverse + ' = ' + pal;
-  //
-  // }
-  //
-  //   return ('Non è palindromo = ') + reverse + ' = ' + pal;
-
-  for (var i = 0; i < pal.length / 2; i++) {
-
-
-    if (pal[i] !== pal[pal.length - 1 - i])
-
-    return ('Non è palindromo = ') + pal;
-  }
-
-    return ('Palindromo = ') + pal;
-
-}
-
-
-
-
+// function palindromo(pal) {
+//
+//   // var reverse = pal.split('').reverse().join('');
+//   //
+//   // if (reverse === pal) {
+//   //
+//   //   return ('Palindromo = ') + reverse + ' = ' + pal;
+//   //
+//   // }
+//   //
+//   //   return ('Non è palindromo = ') + reverse + ' = ' + pal;
+//
+//   for (var i = 0; i < pal.length / 2; i++) {
+//
+//
+//     if (pal[i] !== pal[pal.length - 1 - i])
+//
+//     return ('Non è palindromo = ') + pal;
+//   }
+//
+//     return ('Palindromo = ') + pal;
+//
+// }
 
 
 //////////////////// Pari e Dispari
@@ -43,3 +39,25 @@ function palindromo(pal) {
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+var numero = parseInt( prompt('Inserisci numero da 1 a 5') );
+
+var pariDispari = prompt('Scegli pari o dispari');
+
+var risultato = pariOdispari();
+
+console.log(risultato);
+
+function pariOdispari() {
+    if (numero < 1 && numero > 5) {
+      var numero = parseInt( prompt('HO DETTO DA 1 a 5!!!') );
+    }
+    // Numero random da 1 a 5
+    var numeroPC = Math.floor( Math.random() * 5) + 1;
+
+    if (numero + numeroPC % 2 == 0) {
+      return ('Vince il pari');
+    }
+
+    return ('Vince il dispari')
+}
